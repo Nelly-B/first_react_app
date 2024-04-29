b9ba73757d656c0f1bb07f0a990700b97cc41f02
Nelly-B <alapanelly@gmail.com>
2024-04-29 16:22:36 +0100

    arrow function

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,11 @@
 import { useState } from 'react'
 import './App.css'
-import './nav1.css'
-// import './Expense.css'
-// import Navbar1 from '../components/nav'
-import ExpenseItem from '../components/ExpenseItem'
-import Expenses from '../components/Expenses'
+import Expenses from '../components/Expenses/Expenses'
+// import ExpenseItem from '../components/Expenses/ExpenseItem'
 
- function App() {
-//   const expenses = [
-//     {
-//       id: 'el',
-//       title : 'Toilet Paper',
-//       amount : 94.12,
-//       date : new Date(2024, 4, 12)
-//     },
-//     {
-//       id: 'e2',
-//       title : 'New TV',
-//       amount : 794.12,
-//       date : new Date(2024, 4, 23)
-//     },
-//     {
-//       id: 'e3',
-//       title : 'Car Insurance',
-//       amount : 294.78,
-//       date : new Date(2024, 4, 28)
-//     },
-//     {
-//       id: 'e4',
-//       title : 'New Shirt',
-//       amount : 194.12,
-//       date : new Date(2024, 4, 30)
-//     }
 
-//   ]
+ const App = () => {
+
    return (
     <>
     <div>
